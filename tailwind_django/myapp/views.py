e9eb79b66d96941dcb2e71d8fb75cb70793a967e
Nicholas Kissaun <31455235+Tephrite@users.noreply.github.com>
2022-06-04 22:45:39 +0100

    Add programs page

--- tailwind_django/myapp/views.py
+++ tailwind_django/myapp/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-# Create your views here.
 def what_we_do(request):
     return render(request, 'what_we_do.html')
--- tailwind_django/tailwind_django/urls.py
+++ tailwind_django/tailwind_django/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("what_we_do/", TemplateView.as_view(template_name="what_we_do.html"), name="what_we_do"),
+    path("programs/", TemplateView.as_view(template_name="programs.html"), name="programs"),
 ]
